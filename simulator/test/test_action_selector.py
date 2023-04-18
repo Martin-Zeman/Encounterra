b6bdefb5959bcb8d3af136a1278932f491b69dab
Martin Zeman <zemanm9@gmail.com>
2023-04-18 12:59:03 +0200

    Split the DAG construction tests down to multiple test cases

--- simulator/test/test_action_selector.py
+++ simulator/test/test_action_selector.py
@@ -7,7 +7,8 @@ from simulator.teams import Teams
 from simulator.test.fixtures import combatant1, combatant2, combatant3, teams, effect_tracker, battle_map
 from simulator.actions.action_selector import select_best_action
 
-def test_select_best_action(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
+
+def test_select_best_action_misty_step_and_firebolt(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
     battle_map.build_adjacency_matrix()
     battle_map.set_effect_tracker(effect_tracker)
     effect_tracker.set_battle_map(battle_map)
@@ -35,35 +36,58 @@ def test_select_best_action(battle_map, teams, effect_tracker, combatant1, comba
     dfs.trigger("ms_(2, 3)")
     transitions = dfs.get_available_transitions()
     assert "Staff of Defence on Goblin" not in transitions  # Test that Misty Step actions are also prepended with movement
-    # TODO What about the Dodge here?
     assert "Firebolt on Goblin" in transitions
+    assert "Dodge of Faurung" not in transitions # Even though it's possible, we don't support dodge after Misty Step, as it's very niche
+    assert "Disengage of Faurung" not in transitions # Even though it's possible, we don't support dodge after Misty Step, as it's very niche
     dfs.trigger("Firebolt on Goblin")
     assert len(dfs.get_available_transitions()) == 0
 
-    # Tests regular movement + quickened fireball
-    dfs.reset()
-    assert dfs.state == '0'
-    dfs.trigger("m_(2, 3)")
-    transitions = dfs.get_available_transitions()
-    # Check that we have all the action (except for the Staff attack) available
-    assert 'Quickened Fireball at [ 6 10]' in transitions
-    assert 'Quickened Firebolt on Goblin' in transitions
-    assert 'Quickened Haste on Faurung' in transitions
-    assert 'Fireball at [ 6 10]' in transitions
-    assert 'Firebolt on Goblin' in transitions
-    assert 'Haste on Faurung' in transitions
-    assert 'Dodge of Faurung' not in transitions  # Once you do a regular move, Dodge should not be available
-    assert 'Disengage of Faurung' not in transitions  # Once you do a regular move, Disengage should not be available
-    dfs.trigger("Quickened Fireball at [ 6 10]")
-    transitions = dfs.get_available_transitions()
-    # For the second action, coordinates are not taken into account, but Dodge is included
-    assert 'Staff of Defence on Goblin' in transitions
-    assert 'Firebolt on Goblin' in transitions
-    assert 'Dodge of Faurung' in transitions
-    assert 'Disengage of Faurung' in transitions
+def test_select_best_action_movement_and_quickened_fireball(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
+        battle_map.build_adjacency_matrix()
+        battle_map.set_effect_tracker(effect_tracker)
+        effect_tracker.set_battle_map(battle_map)
+        teams.add_combatant_to_team(combatant1, Teams.Color.BLUE)
+        teams.add_combatant_to_team(combatant2, Teams.Color.RED)
+        # teams.add_combatant_to_team(combatant3, Teams.Color.RED)
+        battle_map.set_combatant_coordinates(combatant1, np.array([1, 3]))
+        battle_map.set_combatant_coordinates(combatant2, np.array([10, 10]))
+        # battle_map.set_combatant_coordinates(combatant3, np.array([2, 3]))
 
+        dfs = select_best_action(combatant1, battle_map)
+        # Tests regular movement + quickened fireball
+        assert dfs.state == '0'
+        dfs.trigger("m_(2, 3)")
+        transitions = dfs.get_available_transitions()
+        # Check that we have all the action (except for the Staff attack) available
+        assert 'Quickened Fireball at [ 6 10]' in transitions
+        assert 'Quickened Firebolt on Goblin' in transitions
+        assert 'Quickened Haste on Faurung' in transitions
+        assert 'Fireball at [ 6 10]' in transitions
+        assert 'Firebolt on Goblin' in transitions
+        assert 'Haste on Faurung' in transitions
+        assert 'Dodge of Faurung' not in transitions  # Once you do a regular move, Dodge should not be available
+        assert 'Disengage of Faurung' not in transitions  # Once you do a regular move, Disengage should not be available
+        dfs.trigger("Quickened Fireball at [ 6 10]")
+        transitions = dfs.get_available_transitions()
+        # For the second action, coordinates are not taken into account, but Dodge is included
+        assert 'Staff of Defence on Goblin' in transitions
+        assert 'Firebolt on Goblin' in transitions
+        assert 'Dodge of Faurung' in transitions
+        assert 'Disengage of Faurung' in transitions
+
+def test_select_best_action_movement_and_fireball(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
+    battle_map.build_adjacency_matrix()
+    battle_map.set_effect_tracker(effect_tracker)
+    effect_tracker.set_battle_map(battle_map)
+    teams.add_combatant_to_team(combatant1, Teams.Color.BLUE)
+    teams.add_combatant_to_team(combatant2, Teams.Color.RED)
+    # teams.add_combatant_to_team(combatant3, Teams.Color.RED)
+    battle_map.set_combatant_coordinates(combatant1, np.array([1, 3]))
+    battle_map.set_combatant_coordinates(combatant2, np.array([10, 10]))
+    # battle_map.set_combatant_coordinates(combatant3, np.array([2, 3]))  # Have to set it for fireball placement
+
+    dfs = select_best_action(combatant1, battle_map)
     # Tests regular movement + fireball
-    dfs.reset()
     assert dfs.state == '0'
     dfs.trigger("m_(2, 3)")
     transitions = dfs.get_available_transitions()
@@ -81,8 +105,19 @@ def test_select_best_action(battle_map, teams, effect_tracker, combatant1, comba
     # For the second action, coordinates are not taken into account
     assert 'Quickened Firebolt on Goblin' in transitions
 
+def test_select_best_action_movement_and_staff_attack(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
+    battle_map.build_adjacency_matrix()
+    battle_map.set_effect_tracker(effect_tracker)
+    effect_tracker.set_battle_map(battle_map)
+    teams.add_combatant_to_team(combatant1, Teams.Color.BLUE)
+    teams.add_combatant_to_team(combatant2, Teams.Color.RED)
+    # teams.add_combatant_to_team(combatant3, Teams.Color.RED)
+    battle_map.set_combatant_coordinates(combatant1, np.array([1, 3]))
+    battle_map.set_combatant_coordinates(combatant2, np.array([10, 10]))
+    # battle_map.set_combatant_coordinates(combatant3, np.array([2, 3]))
+
+    dfs = select_best_action(combatant1, battle_map)
     # Tests regular movement + staff of defence attack
-    dfs.reset()
     assert dfs.state == '0'
     dfs.trigger("m_(9, 10)")
     transitions = dfs.get_available_transitions()
@@ -103,8 +138,20 @@ def test_select_best_action(battle_map, teams, effect_tracker, combatant1, comba
     assert 'Quickened Firebolt on Goblin' in transitions
     assert 'Quickened Haste on Faurung' in transitions
 
+def test_select_best_action_misty_step_and_staff_attack(battle_map, teams, effect_tracker, combatant1, combatant2,
+                                                      combatant3):
+    battle_map.build_adjacency_matrix()
+    battle_map.set_effect_tracker(effect_tracker)
+    effect_tracker.set_battle_map(battle_map)
+    teams.add_combatant_to_team(combatant1, Teams.Color.BLUE)
+    teams.add_combatant_to_team(combatant2, Teams.Color.RED)
+    # teams.add_combatant_to_team(combatant3, Teams.Color.RED)
+    battle_map.set_combatant_coordinates(combatant1, np.array([1, 3]))
+    battle_map.set_combatant_coordinates(combatant2, np.array([10, 10]))
+    # battle_map.set_combatant_coordinates(combatant3, np.array([2, 3]))
+
+    dfs = select_best_action(combatant1, battle_map)
     # Tests Misty Step movement + staff of defence attack
-    dfs.reset()
     assert dfs.state == '0'
     dfs.trigger("ms_(9, 10)")
     transitions = dfs.get_available_transitions()
@@ -114,14 +161,25 @@ def test_select_best_action(battle_map, teams, effect_tracker, combatant1, comba
     dfs.trigger("Staff of Defence on Goblin")
     assert len(dfs.get_available_transitions()) == 0
 
+def test_select_best_action_dodge_and_movement_and_quickened_spell(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
+    battle_map.build_adjacency_matrix()
+    battle_map.set_effect_tracker(effect_tracker)
+    effect_tracker.set_battle_map(battle_map)
+    teams.add_combatant_to_team(combatant1, Teams.Color.BLUE)
+    teams.add_combatant_to_team(combatant2, Teams.Color.RED)
+    # teams.add_combatant_to_team(combatant3, Teams.Color.RED)
+    battle_map.set_combatant_coordinates(combatant1, np.array([1, 3]))
+    battle_map.set_combatant_coordinates(combatant2, np.array([10, 10]))
+    # battle_map.set_combatant_coordinates(combatant3, np.array([2, 3]))
+
+    dfs = select_best_action(combatant1, battle_map)
     # Tests Dodge + movement + a quickened spell
-    dfs.reset()
     assert dfs.state == '0'
     dfs.trigger("Dodge of Faurung")
     assert dfs.state == 'Dodged'
     transitions = dfs.get_available_transitions()
     assert "m_(7, 3)" in transitions
-    assert "ms_(2, 3)" not in transitions  # Even though it's possible, we don't support Misty Step after Dodge, as it doesn't make muche sense
+    assert "ms_(2, 3)" not in transitions  # Even though it's possible, we don't support Misty Step after Dodge, as it's very niche
     dfs.trigger("m_(7, 3)")
     assert dfs.state == "do_(7, 3)"
     transitions = dfs.get_available_transitions()
@@ -131,8 +189,19 @@ def test_select_best_action(battle_map, teams, effect_tracker, combatant1, comba
     dfs.trigger("Quickened Haste on Faurung")
     assert len(dfs.get_available_transitions()) == 0
 
+def test_select_best_action_disengage_and_movement_and_quickened_spell(battle_map, teams, effect_tracker, combatant1, combatant2, combatant3):
+    battle_map.build_adjacency_matrix()
+    battle_map.set_effect_tracker(effect_tracker)
+    effect_tracker.set_battle_map(battle_map)
+    teams.add_combatant_to_team(combatant1, Teams.Color.BLUE)
+    teams.add_combatant_to_team(combatant2, Teams.Color.RED)
+    # teams.add_combatant_to_team(combatant3, Teams.Color.RED)
+    battle_map.set_combatant_coordinates(combatant1, np.array([1, 3]))
+    battle_map.set_combatant_coordinates(combatant2, np.array([10, 10]))
+    # battle_map.set_combatant_coordinates(combatant3, np.array([2, 3]))
+
+    dfs = select_best_action(combatant1, battle_map)
     # Tests Disengage + movement + a quickened spell
-    dfs.reset()
     assert dfs.state == '0'
     dfs.trigger("Disengage of Faurung")
     assert dfs.state == 'Disengaged'
@@ -147,4 +216,3 @@ def test_select_best_action(battle_map, teams, effect_tracker, combatant1, comba
     assert 'Quickened Haste on Faurung' in transitions
     dfs.trigger("Quickened Firebolt on Goblin")
     assert len(dfs.get_available_transitions()) == 0
-
