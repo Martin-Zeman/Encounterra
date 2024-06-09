68805e6dc7ef2202322d68b7b32d7f077dbc9d81
Martin Zeman <zemanm9@gmail.com>
2024-06-09 12:33:59 +0200

    Building Dockerfile for batch

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ GIT_COMMIT := $(shell git rev-parse --short HEAD)
 GIT_BRANCH_SANITIZED := $(subst /,-,${GIT_BRANCH})
 
 DOCKER_CONTAINER = encounterra-core
+DOCKER_BATCH_CONTAINER = encounterra-batch_core
 DOCKER_SANDBOX_REMOTE = ${ACCOUNT_NR}.dkr.ecr.eu-west-1.amazonaws.com
 
 docker.build:
@@ -39,6 +40,35 @@ docker.build:
 		echo "* short commit nr: ${GIT_COMMIT}"; \
 	fi
 
+docker.build_batch:
+	@echo ""
+	@echo "Building Batch container..."
+	aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${DOCKER_SANDBOX_REMOTE}
+	docker build -t ${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED} --build-arg APP_VERSION=${GIT_COMMIT} --network host -f Dockerfile_batch .
+	docker tag ${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED} ${DOCKER_BATCH_CONTAINER}:latest
+	@if [ -z "${GIT_STATUS}" ]; then \
+        docker tag ${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED} ${DOCKER_BATCH_CONTAINER}:${GIT_COMMIT}; \
+        docker tag ${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED} ${DOCKER_SANDBOX_REMOTE}/${DOCKER_BATCH_CONTAINER}:${GIT_COMMIT}; \
+    fi
+	@echo ""
+	@echo "*"
+	@echo "*"
+	@echo "* Docker container built"
+	@echo "*"
+	@echo "*  tagged: ${FONT_BOLD}${DOCKER_BATCH_CONTAINER}:latest${FONT_NORMAL}"
+	@echo "*  tagged: ${FONT_BOLD}${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED}${FONT_NORMAL}"
+	@if [ -z "${GIT_STATUS}" ]; then \
+		docker tag ${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED} ${DOCKER_BATCH_CONTAINER}:${GIT_COMMIT}; \
+		docker tag ${DOCKER_BATCH_CONTAINER}:${GIT_BRANCH_SANITIZED} ${DOCKER_SANDBOX_REMOTE}/${DOCKER_BATCH_CONTAINER}:${GIT_COMMIT}; \
+	else \
+		echo "Workspace is dirty!"; \
+	fi
+	@echo "*"
+	@echo "*"
+	@if [ -z "${GIT_STATUS}" ]; then \
+		echo "* short commit nr: ${GIT_COMMIT}"; \
+	fi
+
 docker.push:
 	@echo ""
 	@echo "... pushing containers..."
@@ -56,6 +86,30 @@ docker.push:
 	@echo "* Container pushed"
 	@echo "*"
 	@echo "*  pushed: ${DOCKER_SANDBOX_REMOTE}/${DOCKER_CONTAINER}:${GIT_COMMIT}"
+	@echo "* "
+	@if [ -z "${GIT_STATUS}" ]; then \
+        echo "* short commit nr: ${GIT_COMMIT}"; \
+    fi
+	@echo "*"
+	@echo ""
+
+docker.push_batch:
+	@echo ""
+	@echo "... pushing containers..."
+	@echo ""
+	@if [ ! -z "${GIT_STATUS}" ]; then \
+        echo "Workspace is dirty, cannot push!"; \
+        echo ""; \
+        exit 1; \
+    fi
+	docker push ${DOCKER_SANDBOX_REMOTE}/${DOCKER_BATCH_CONTAINER}:${GIT_COMMIT}
+
+	@echo ""
+	@echo "*"
+	@echo "*"
+	@echo "* Container pushed"
+	@echo "*"
+	@echo "*  pushed: ${DOCKER_SANDBOX_REMOTE}/${DOCKER_BATCH_CONTAINER}:${GIT_COMMIT}"
 	@echo "* "
 	@if [ -z "${GIT_STATUS}" ]; then \
         echo "* short commit nr: ${GIT_COMMIT}"; \
---
+++ batch_entrypoint.py
@@ -0,0 +1,169 @@
+import argparse
+import pickle
+import time
+import traceback
+import re
+from datetime import datetime
+
+from simulator.logging.custom_logger import CustomLogger
+from simulator.misc import Statistics
+from simulator.resources import ResourceDepletionLevel
+from simulator.session import Session
+from simulator.teams import Teams
+from simulator.battle_map import Map
+
+import boto3
+import logging
+import os
+
+
+dynamodb = boto3.client('dynamodb', region_name='eu-west-1')  # TODO remove the region
+s3 = boto3.client('s3')
+results_bucket_name = "encounterra-simulation-results"
+crash_bucket_name = "encounterra-simulation-crashes"
+local_log_file_path = "/tmp/log.txt"
+
+
+def simplify_movements(log_lines):
+    simplified_logs = []
+    previous_entity = None
+    previous_line = None
+
+    # Adjusted pattern to match the movement lines, including the optional space
+    move_pattern = re.compile(r"^(.*?moved to \[\s?\d+\s+\d+\])$")
+
+    for line in log_lines:
+        move_match = move_pattern.match(line)
+
+        if move_match:
+            # Extract the entity part before " moved to"
+            current_entity = move_match.group(1).split(" moved to")[0]
+
+            if current_entity == previous_entity:
+                previous_line = line  # Update the previous line to the current one
+            else:
+                if previous_line:
+                    simplified_logs.append(previous_line)  # Append the previous movement line
+                previous_entity = current_entity
+                previous_line = line
+        else:
+            if previous_line:
+                simplified_logs.append(previous_line)  # Append the last movement line
+                previous_entity = None
+                previous_line = None
+            simplified_logs.append(line)
+
+    if previous_line:
+        simplified_logs.append(previous_line)  # Append the last movement line if any
+
+    return simplified_logs
+
+
+if os.path.exists(local_log_file_path):
+    os.remove(local_log_file_path)
+CustomLogger(logging.INFO, False, local_log_file_path)
+logger = logging.getLogger("Encounterra")
+Map.reset_singleton()
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--blue', nargs='+', type=str)
+parser.add_argument('--red', nargs='+', type=str)
+parser.add_argument('--combatant-placement', type=int)
+parser.add_argument('--blue-depletion-level', type=int)
+parser.add_argument('--red-depletion-level', type=int)
+parser.add_argument('--iteration', type=int)
+parser.add_argument('--map-type', type=str)
+parser.add_argument('--job-id', type=str)
+parser.add_argument('--index', type=str)
+args = parser.parse_args()
+
+blue_team = args.blue
+red_team = args.red
+combatant_placement = args.combatant_placement
+blue_depletion_level = args.blue_depletion_level
+red_depletion_level = args.red_depletion_level
+map_type = args.map_type
+job_id = args.job_id
+index = args.index
+
+date_suffix = datetime.now().strftime("%Y%m%d-%H%M")
+job_id_with_suffix = f"{job_id}_{date_suffix}"
+subdirectory = f"{job_id}/{index}/"
+
+session = Session()
+session.set_placement_scenario(Session.PlacementScenario(combatant_placement))
+session.place_terrain_and_obstacles(map_type)
+match blue_depletion_level:
+    case ResourceDepletionLevel.FULLY_RESTED.value:
+        logger.info("Blue Team is fully rested")
+    case ResourceDepletionLevel.PARTIALLY_DEPLETED.value:
+        logger.info("Blue Team is partially depleted")
+    case ResourceDepletionLevel.FULLY_DEPLETED.value:
+        logger.info("Blue Team is fully depleted")
+    case _:
+        logger.error("Unknown resource depletion level for the Blue Team")
+match red_depletion_level:
+    case ResourceDepletionLevel.FULLY_RESTED.value:
+        logger.info("Red Team is fully rested")
+    case ResourceDepletionLevel.PARTIALLY_DEPLETED.value:
+        logger.info("Red Team is partially depleted")
+    case ResourceDepletionLevel.FULLY_DEPLETED.value:
+        logger.info("Red Team is fully depleted")
+    case _:
+        logger.error("Unknown resource depletion level for the Red Team")
+for blue_combatant in blue_team:
+    session.add_combatant(int(blue_combatant), Teams.Color.BLUE, ResourceDepletionLevel(blue_depletion_level))
+for red_combatant in red_team:
+    session.add_combatant(int(red_combatant), Teams.Color.RED, ResourceDepletionLevel(red_depletion_level))
+session.set_num_simulations(1)
+try:
+    result = session.simulate(parallel=False)
+
+    with open(local_log_file_path, 'r') as file:
+        log_lines = file.readlines()
+    simplified_logs = simplify_movements(log_lines)
+    with open(local_log_file_path, 'w') as file:
+        file.writelines(simplified_logs)
+
+    blue_victory = result[Teams.Color.BLUE][Statistics.VICTORIES]
+
+    s3_object_key = subdirectory + f'{"blue" if blue_victory == 1 else "red"}_victory_log.txt'
+    s3.upload_file(local_log_file_path, results_bucket_name, s3_object_key)
+    # logger.info(f"{job_id}:{index} SUCCESS")
+    # return {
+    #     'blue_victory': result[Teams.Color.BLUE][Statistics.VICTORIES],
+    #     'red_victory': result[Teams.Color.RED][Statistics.VICTORIES],
+    #     'blue_at_least_one_died': result[Teams.Color.BLUE][Statistics.AT_LEAST_ONE_DIED],
+    #     'red_at_least_one_died': result[Teams.Color.RED][Statistics.AT_LEAST_ONE_DIED],
+    #     'blue_at_least_two_died': result[Teams.Color.BLUE][Statistics.AT_LEAST_TWO_DIED],
+    #     'red_at_least_two_died': result[Teams.Color.RED][Statistics.AT_LEAST_TWO_DIED],
+    #     'blue_at_least_three_died': result[Teams.Color.BLUE][Statistics.AT_LEAST_THREE_DIED],
+    #     'red_at_least_three_died': result[Teams.Color.RED][Statistics.AT_LEAST_THREE_DIED],
+    # }
+except Exception as e:
+    error_message = traceback.format_exc()
+    logger.error(f"{job_id}:{index} FAILURE: {error_message}")
+    try:
+        timestamp = int(time.time())
+        logger.info(f"Serializing error with timestamp: {timestamp}")
+        battle_map_data = f'battle_map_data_{timestamp}.pkl'
+        battle_map_data_path = '/tmp/' + battle_map_data
+        session_data = f'session_{timestamp}.pkl'
+        session_data_path = '/tmp/' + session_data
+        exception_data = f'exception_{timestamp}.txt'
+        exception_data_path = '/tmp/' + exception_data
+        with open(battle_map_data_path, 'wb') as f:
+            pickle.dump(Map.serialize_data(), f)
+        with open(session_data_path, 'wb') as f:
+            pickle.dump(session.serialize_data(), f)
+        with open(exception_data_path, 'w') as f:
+            f.write(f"Fuzzy test with Blue team {blue_team} and Red team {red_team} raised an exception:\n{e}")
+        s3.upload_file(battle_map_data_path, crash_bucket_name, f"{job_id_with_suffix}/" + battle_map_data)
+        s3.upload_file(session_data_path, crash_bucket_name, f"{job_id_with_suffix}/" + session_data)
+        s3.upload_file(exception_data_path, crash_bucket_name, f"{job_id_with_suffix}/" + exception_data)
+        s3.upload_file(local_log_file_path, crash_bucket_name, f"{job_id_with_suffix}/log_{timestamp}.txt")
+    except Exception as serialization_e:
+        logger.error(f"Failed to serialize and upload objects: {serialization_e}")
+    exit(1)
