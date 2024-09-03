ccb90e536d5fc11c33afe3037162b6264d02e78b
Martin Zeman <zemanm9@gmail.com>
2024-09-03 15:50:47 +0200

    Add spellslots

--- c++/core/resources.hpp
+++ c++/core/resources.hpp
@@ -17,4 +17,48 @@ namespace enc
     ROUND,
     NEVER
   };
+
+  class Resource
+  {
+  public:
+    Resource(ResourceRefreshType refreshType) : _refreshType(refreshType) {}
+    virtual ~Resource() = default;
+
+    virtual bool hasResource(int level) = 0;
+    virtual int getResource(int level) = 0;
+    virtual void useResource(int amount = 1) = 0;
+    virtual void reset() = 0;
+    virtual void depleteResource(ResourceDepletionLevel level) = 0;
+
+  protected:
+    ResourceRefreshType _refreshType;
+  };
+
+  class Uses : public Resource
+  {
+  public:
+    Uses(int uses, ResourceRefreshType refreshType = ResourceRefreshType::LONG_REST) : Resource(refreshType), _currUses(uses), _maxUses(uses) {}
+
+    bool hasResource(int level) override { return _currUses > 0; } // level is ignored in this case
+    int getResource(int level) override { return _currUses; }  // level is ignored in this case
+    void useResource(int uses = 1) override { _currUses -= uses; }
+    void addResource(int uses = 1) { _currUses += uses; }
+    void setResource(int uses) { _currUses = uses; }
+    bool isInf() const { return _currUses == INFINITY; }
+    void reset() override { _currUses = _maxUses; }
+
+    void depleteResource(ResourceDepletionLevel level) override
+    {
+      switch(level)
+        {
+        case ResourceDepletionLevel::FULLY_DEPLETED: _currUses = 0; break;
+        case ResourceDepletionLevel::PARTIALLY_DEPLETED: _currUses = _maxUses / 2; break;
+        default: break;
+        }
+    }
+
+  private:
+    int _currUses;
+    int _maxUses;
+  };
 }
